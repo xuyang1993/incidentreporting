8891fc9aefe8428a39b99de9efbc3c5a971d86fb
xuyang <chuckyoung93@gmail.com>
2017-08-19 13:26:38 +0800

    Initial Commit

---
+++ Incident Reporting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Incident Reporting
+//
+//  Created by XuYang on 19/8/17.
+//  Copyright © 2017 XuYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
